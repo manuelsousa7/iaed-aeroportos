88cb786bb41904b82c34c3b942b3e1c595f5009b
goncalo <goncalo.cool@gmail.com>
2016-03-16 18:43:13 +0000

    funcao leString

--- codigo_fonte/codigo_fonte.c
+++ codigo_fonte/codigo_fonte.c
@@ -2,15 +2,27 @@
 #include <string.h>
 #include <math.h>
 
+#define MAXSTRLEN 100
+
+char leString();
+
 int main()
 {
-	lel
-	return 1;
+	
+	return 0;
 }
 
-int achas(int a,int b)
-{
+char leString(){
+	char str[MAXSTRLEN], c;
+	int i = 0;
 
-	printf("%d",a+b);
-	return a+b;
+	if ((c = getchar()) == '\n')
+		c = getchar();
+	while (c != '\n' && c != EOF){
+		str[i++] = c;
+		c = getchar();
+	}
+	str[i] = '\0';
+	
+	return str;
 }
