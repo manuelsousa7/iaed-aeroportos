29fe98b20f54d87ed7eba8dbe5606449dcb20ca8
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2016-03-19 16:31:03 +0000

    Comentarios Atualizados

--- codigo_fonte/main.c
+++ codigo_fonte/main.c
@@ -1,12 +1,4 @@
-/*                                                         .:INSTITUTO SUPERIOR TECNICO:.
-											Programa Realizado Por:
-													 - Gonçalo Marques (84719)
-													 - Manuel Sousa (84740)
-											Tema: Redes de Aeroportos
-											Curso: Licenciatura de Engenharia Informática e Computadores (LEIC-T)
-											Cadeira: Introdução aos Algoritmos e Estruturas de Dados
-											Data de Entrega: 02/04/2016
-*/
+// Grupo 11 (TG011) - Gonçalo Marques (84719) & Manuel Sousa (84740)
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
